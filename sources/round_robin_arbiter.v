65764c5b8fa8e7d739cab4e093efc573acc1391b
SreekanthBillupati <116333193+SreekanthBillupati@users.noreply.github.com>
2026-02-12 21:47:19 +0530

    Update round_robin_arbiter.v

--- sources/round_robin_arbiter.v
+++ sources/round_robin_arbiter.v
@@ -12,5 +12,11 @@ module round_robin_arbiter (
 );
 
  //internal logic here
+  assign gnt_o[0] = req_i[0];
 
+  genvar i;
+  for (i=1; i<NUM_PORTS; i=i+1) begin
+    assign gnt_o[i] = req_i[i] & ~(|gnt_o[i-1:0]);
+  end
+  
 endmodule
